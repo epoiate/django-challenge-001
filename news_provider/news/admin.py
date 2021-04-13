7315567cfa575ca68e548db60487d8329c1f0415
Eduardo Poiate <eduardopoiate@gmail.com>
2021-04-13 20:28:56 -0300

    New models for Author and Article
    - Added models for Author and Article
    - Added admin pages for Author and Article
    - Added Pillow in the dependency list

--- news_provider/news/admin.py
+++ news_provider/news/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Author, Article
 
 # Register your models here.
+
+
+@admin.register(Author)
+class Authors(admin.ModelAdmin):
+    """Admin class for Authors"""
+
+
+@admin.register(Article)
+class Articles(admin.ModelAdmin):
+    """Admin class for Articles"""
--- news_provider/news/models.py
+++ news_provider/news/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Author(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    picture = models.ImageField()
+
+
+class Article(models.Model):
+    """"""
+
+    category = models.CharField(max_length=20)
+    title = models.CharField(max_length=100)
+    summary = models.CharField(max_length=200)
+    firstParagraph = models.TextField()
+    body = models.TextField()
