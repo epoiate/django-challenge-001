70ee9e9cd126333ff56ac4894c3f4359f1fc0529
Eduardo Poiate <eduardopoiate@gmail.com>
2021-04-13 12:31:11 -0300

    Created app News and Users
    - Created app Users
    - Created app News
    - Added Django REST Framework to the list of dependencies

--- news_provider/news_provider/settings.py
+++ news_provider/news_provider/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m5g-9c6%=%_s(+#$*(=ah#s)92rfgypx*p+qy!bw1en9^z5zv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'users',
+    'news',
 ]
 
 MIDDLEWARE = [
