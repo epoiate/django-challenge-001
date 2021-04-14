22965ed5ff00f940781675c3b7e0c7520eaa4f3e
Eduardo Poiate <eduardopoiate@gmail.com>
2021-04-13 22:35:49 -0300

    Added support for media files
    - Added media folder
    - Added default picture for Author

--- news_provider/news/models.py
+++ news_provider/news/models.py
@@ -6,14 +6,15 @@ from django.contrib.auth.models import User
 
 
 class Author(models.Model):
+    """Author model"""
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    picture = models.ImageField()
+    picture = models.ImageField(upload_to='images/%Y/%m/%d/', default='images/generic_user.png')
 
 
 class Article(models.Model):
-    """"""
+    """Article model"""
 
     category = models.CharField(max_length=20)
     title = models.CharField(max_length=100)
