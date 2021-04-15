f27867e9cd693cfbcd9529e9d82e68c88e8c91ba
Eduardo Poiate <eduardopoiate@gmail.com>
2021-04-14 22:07:11 -0300

    Created CustomUser
    - New model CustomUser
    - CustomUser is now the default user model
    - Changed Article.id to UUIDField

--- news_provider/news/models.py
+++ news_provider/news/models.py
@@ -1,14 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+import uuid
 
 
 # Create your models here.
 
 
 class Author(models.Model):
-    """Author model"""
+    """
+    Author model.
+    Assuming not all users are Authors
+    """
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    id = models.OneToOneField(get_user_model(), primary_key=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     picture = models.ImageField(upload_to='images/%Y/%m/%d/', default='images/generic_user.png')
 
@@ -16,6 +20,7 @@ class Author(models.Model):
 class Article(models.Model):
     """Article model"""
 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
     category = models.CharField(max_length=20)
     title = models.CharField(max_length=100)
     summary = models.CharField(max_length=200)
--- news_provider/users/models.py
+++ news_provider/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    """Default User model with UUID as id"""
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
