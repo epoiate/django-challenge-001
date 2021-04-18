e510016598b738cf1a1e370b23380c6f66a20b2f
Eduardo Poiate <eduardopoiate@gmail.com>
2021-04-17 22:08:18 -0300

    Corrected field 'author' in model Article
    - Model Article was lacking field 'author'.

--- news_provider/news/models.py
+++ news_provider/news/models.py
@@ -1,6 +1,7 @@
-from django.db import models
-from django.contrib.auth import get_user_model
 import uuid
+
+from django.contrib.auth import get_user_model
+from django.db import models
 
 
 # Create your models here.
@@ -16,13 +17,20 @@ class Author(models.Model):
     name = models.CharField(max_length=50)
     picture = models.ImageField(upload_to='images/%Y/%m/%d/', default='images/generic_user.png')
 
+    def __str__(self):
+        return self.name
+
 
 class Article(models.Model):
     """Article model"""
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     category = models.CharField(max_length=20)
     title = models.CharField(max_length=100)
     summary = models.CharField(max_length=200)
     firstParagraph = models.TextField()
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
