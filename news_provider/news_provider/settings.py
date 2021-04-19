a5962caa98b732f25f5e9ccf9cf1c31fa5f59dc8
Eduardo Poiate <eduardopoiate@gmail.com>
2021-04-18 22:24:44 -0300

    Created Docker container for Nginx
    - Created Dockerfile.
    - Created docker-compose.prod.yml.

--- news_provider/news_provider/settings.py
+++ news_provider/news_provider/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m5g-9c6%=%_s(+#$*(=ah#s)92rfgypx*p+qy!bw1en9^z5zv('
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-m5g-9c6%=%_s(+#$*(=ah#s)92rfgypx*p+qy!bw1en9^z5zv(')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', False)
 
 ALLOWED_HOSTS = ['*']
 
